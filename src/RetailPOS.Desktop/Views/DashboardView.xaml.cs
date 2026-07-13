3f4cfd39a86f51cbeb458ee905f8cdd17bad3c87
higwon <higwon2@gmail.com>
2026-07-14 00:56:23 +0900

    style: refine cashier workflows

--- src/RetailPOS.Desktop/Views/DashboardView.xaml.cs
+++ src/RetailPOS.Desktop/Views/DashboardView.xaml.cs
@@ -1,20 +1,26 @@
 using System.Windows.Controls;
 using RetailPOS.Desktop.ViewModels;
+using RetailPOS.Desktop.Workflow;
 
 namespace RetailPOS.Desktop.Views;
 
 public partial class DashboardView : UserControl
 {
     private readonly DashboardViewModel _viewModel;
+    private readonly CashierWorkflowNavigator _workflowNavigator;
 
-    public DashboardView(DashboardViewModel viewModel)
+    public DashboardView(DashboardViewModel viewModel, CashierWorkflowNavigator workflowNavigator)
     {
         InitializeComponent();
         _viewModel = viewModel;
+        _workflowNavigator = workflowNavigator;
         DataContext = viewModel;
         Loaded += OnLoaded;
     }
 
     private async void OnLoaded(object sender, System.Windows.RoutedEventArgs e) =>
         await _viewModel.LoadAsync();
+
+    private void OnBack(object sender, System.Windows.RoutedEventArgs e) =>
+        _workflowNavigator.Reset(CashierWorkflowScreen.Register);
 }
--- src/RetailPOS.Desktop/Views/StatusView.xaml.cs
+++ src/RetailPOS.Desktop/Views/StatusView.xaml.cs
@@ -1,17 +1,20 @@
 using System.Windows.Controls;
 using RetailPOS.Desktop.ViewModels;
+using RetailPOS.Desktop.Workflow;
 
 namespace RetailPOS.Desktop.Views;
 
 public partial class StatusView : UserControl
 {
     private readonly StatusViewModel _viewModel;
+    private readonly CashierWorkflowNavigator _workflowNavigator;
     private bool _loadedOnce;
 
-    public StatusView(StatusViewModel viewModel)
+    public StatusView(StatusViewModel viewModel, CashierWorkflowNavigator workflowNavigator)
     {
         InitializeComponent();
         _viewModel = viewModel;
+        _workflowNavigator = workflowNavigator;
         DataContext = viewModel;
         Loaded += OnLoaded;
     }
@@ -27,4 +30,7 @@ public partial class StatusView : UserControl
         Loaded -= OnLoaded;
         await _viewModel.LoadAsync();
     }
+
+    private void OnBack(object sender, System.Windows.RoutedEventArgs e) =>
+        _workflowNavigator.Reset(CashierWorkflowScreen.Register);
 }
